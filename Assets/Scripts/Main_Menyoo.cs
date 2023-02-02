fee72f65d56ef1f1c4a791687dbda9209b02313f
liquidchung <77459363+liquidchung@users.noreply.github.com>
2023-02-02 16:56:55 -0500

    Menus
    added main menu, pause menu, and quit buttons

---
+++ Assets/Scripts/Main_Menyoo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menyoo : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitteded");
+        Application.Quit();
+    }
+}
